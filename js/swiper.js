bacdbaae855c3a168363724581cc23758f24de5c
Danang Setiadi <130360563+danangstdi@users.noreply.github.com>
2023-11-07 17:59:38 +0700

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    520: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 75,
+    },
+  },
+});
